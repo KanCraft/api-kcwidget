00a6b6167089026a6d4a8557d938f53695b4ab96
otiai10 <otiai10@gmail.com>
2013-09-12 19:15:36 +0900

    refactor

--- handler.py
+++ handler.py
@@ -1,10 +1,18 @@
 import json
+import urllib
+import util
+import ocr
 
 class Handler:
   def __init__(self,req):
     self.req = req
-    print(self.req.method)
 
   def handle(self):
-    body = json.dumps({'result' : 'hoge' })
+
+    params = util.parsePostData(self.req.data)
+    data    = urllib.unquote(params['imgBin'])
+    binary  = data.replace('data:image/png;base64,','')
+    text = ocr.from_binary(binary)
+
+    body = json.dumps({'result' : text })
     return (body,200)
--- minimum.py
+++ minimum.py
@@ -1,4 +1,3 @@
-# TODO : Refactor
 # TODO : Security
 # TODO : Content-Type : application/json
 
@@ -8,48 +7,18 @@
 from flask import Flask
 from flask import request
 from flask import make_response
-import urllib
-import json
 
 # --- my modules
 import conf
-import ocr
-import util
-
 from handler import Handler
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-  return "This GET '/'"
-
 @app.route('/upload', methods=['POST'])
 def upload_file():
-
-  if request.method != 'POST':
-    return 'Method must be "POST"'
- 
-  if request.data is not None:
-    params = util.parsePostData(request.data)
- 
-  data    = urllib.unquote(params['imgBin'])
-  binary  = data.replace('data:image/png;base64,','')
- 
-  txt = ocr.from_binary(binary)
- 
-  if txt:
-
-    (body, status) = Handler(request).handle()
-
-    resp = make_response(body, status)
-    return resp
-
-  return "Saved?"
-
-@app.route('/check', methods=['GET'])
-def check():
-  return "<img src='./uploaded_file.png'>";
+  (body, status) = Handler(request).handle()
+  resp = make_response(body, status)
+  return resp
 
 if __name__ == "__main__":
   app.run(host=conf.host,port=conf.port)
