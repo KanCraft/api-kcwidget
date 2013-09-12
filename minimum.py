0c588823120b13ff909b478bbd3f3e9de2753e76
otiai10 <otiai10@gmail.com>
2013-09-12 17:05:12 +0900

    separate OCR process

--- minimum.py
+++ minimum.py
@@ -4,23 +4,14 @@
 
 # Python Version 2.6.6
 
+# --- modules
 from flask import Flask
 from flask import request
-
-# --- OCR ---
-from PIL import Image
-import pyocr
-import pyocr.builders
-
-# --- StringIO ---
-from StringIO import StringIO
-
-# --- urllib
 import urllib
-import base64
 
-# --- my module
-from conf import *
+# --- my modules
+import conf
+import ocr
 
 def parsePostData(datastr):
   res = {}
@@ -46,20 +37,11 @@ def upload_file():
 
   data    = urllib.unquote(params['imgBin'])
   binary  = data.replace('data:image/png;base64,','')
-  decoded = base64.b64decode(binary)
 
-  img =  Image.open(StringIO(decoded))
+  txt = ocr.from_binary(binary)
 
-  """ OCR  """
-  tools = pyocr.get_available_tools()
-  tool = tools[0]
-  langs = tool.get_available_languages()
-  txt = tool.image_to_string(img,
-                            lang='eng',
-                            builder=pyocr.builders.TextBuilder())
   if txt:
     return txt
-  """ OCR END """
 
   return "Saved?"
 
@@ -68,4 +50,4 @@ def check():
   return "<img src='./uploaded_file.png'>";
 
 if __name__ == "__main__":
-  app.run(host=host,port=port)
+  app.run(host=conf.host,port=conf.port)
