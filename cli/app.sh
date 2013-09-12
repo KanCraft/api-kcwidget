1633ede3e7816969e058bada16c7d62d653870ac
otiai10 <otiai10@gmail.com>
2013-09-12 20:22:30 +0900

    cli

---
+++ cli/app.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+print_help()
+{
+  echo "\tstart\t起動"
+  echo "\tstop\t終了"
+  exit 0
+}
+
+# script for run the daemon
+if [ $# -lt 1 ]; then
+  print_help
+fi
+
+cur_date=`date '+%Y.%m%d'`
+log_path='log/'$cur_date'.log'
+
+case $1 in
+  "start" )
+    nohup python $PWD/minimum.py >> $log_path &
+    sleep 1s
+    break;;
+  "stop" )
+    ps aux | grep python | grep $PWD/minimum.py | grep -v grep | awk '{print $2}' | xargs kill -9
+    sleep 1s
+    break;;
+  "help" )
+    echo "\033[0;33m"
+    print_help
+    break;;
+  * )
+    print_help
+esac
