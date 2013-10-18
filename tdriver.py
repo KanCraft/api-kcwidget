bb82a32fd4ecd591d391bdd6d23243b2fb3bf284
otiai10 <otiai10@gmail.com>
2013-10-18 23:17:39 +0900

    improve tdriver

--- tdriver.py
+++ tdriver.py
@@ -1,12 +1,12 @@
 import ocr,base64,sys,re,os
 
 def parse_arg():
-  res = {'file':''}
-  for val in sys.argv:
-    if re.search('=', val) is None:
-      continue
-    k,v = val.split('=')
-    res[k] = v
+  res = {'file':'all'}
+  if '--file' in sys.argv:
+    i = sys.argv.index('--file')
+    if len(sys.argv) < i + 1:
+      exit(1)
+    res['file'] = sys.argv[i + 1]
   return res
 
 def exec_by_file(fpath):
