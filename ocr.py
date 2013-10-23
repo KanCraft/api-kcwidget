5e78d1ed8eb224090062a13a3e17f2f52a2a67b7
otiai10 <otiai10@gmail.com>
2013-10-23 20:48:14 +0900

    tuning enhancement

--- ocr.py
+++ ocr.py
@@ -4,47 +4,78 @@ from PIL import ImageEnhance
 from lib import pyocr
 from lib.pyocr import builders
 from StringIO import StringIO
-import base64
+import base64,re
+
+# settings
+tools = pyocr.get_available_tools()
+tool = tools[0]
+lang = 'eng'
+builder = pyocr.builders.TextBuilder()
+
+def is_correct(text):#bool
+  correct_pattern = r'^[0-9]{2}:[0-9]{2}:[0-9]{2}$'
+  if re.search(correct_pattern, text) is None:
+    return False
+  return True
+
+def contrast_loop(img):# string
+  for rate in range(10):
+    enhancing = ImageEnhance.Contrast(img)
+    img = enhancing.enhance(2**rate)
+    txt = tool.image_to_string(img,lang=lang,builder=builder)
+    if is_correct(txt):
+      return txt
+  return txt
+
+def sharpness_loop(img):# string
+  for rate in range(10):
+    enhancing = ImageEnhance.Sharpness(img)
+    img = enhancing.enhance(1/(2**rate))
+    txt = tool.image_to_string(img,lang=lang,builder=builder)
+    if is_correct(txt):
+      return txt
+  return txt
 
 def from_binary(binary, debug=False):
-
-  # settings
-  tools = pyocr.get_available_tools()
-  tool = tools[0]
-  lang = 'eng'
-  builder = pyocr.builders.TextBuilder()
-
   decoded = base64.b64decode(binary)
   img =  Image.open(StringIO(decoded), "r")
 
+
+  # AT FIRST : try raw image
   txt = tool.image_to_string(img,lang=lang,builder=builder)
   if debug:
     print("RawImage\t: " + txt)
-  if txt != "":
+  if is_correct(txt):
     return txt
 
+  # Basically, convert to monocolor
   img = img.convert("L")
   txt = tool.image_to_string(img,lang=lang,builder=builder)
   if debug:
     print("ConvertL\t: " + txt)
-  if txt != "":
+  if is_correct(txt):
     return txt
 
-  # ENHANCE CONTRAST
+  # Basic enhancement
   enhancing = ImageEnhance.Contrast(img)
-  img2 = enhancing.enhance(128)
-  #img = img2
-  txt = tool.image_to_string(img2,lang=lang,builder=builder)
+  img = enhancing.enhance(4)
+  enhancing = ImageEnhance.Sharpness(img)
+  img = enhancing.enhance(1/4)
+  txt = tool.image_to_string(img,lang=lang,builder=builder)
   if debug:
-    print("Contrast\t: " + txt)
-  if txt != "":
+    print("BasicEnhance\t: " + txt)
+  if is_correct(txt):
     return txt
 
-  # TODO: formatting should be by application
-  enhancing = ImageEnhance.Sharpness(img2)
-  img3 = enhancing.enhance(1/16)
-  txt = tool.image_to_string(img3,lang=lang,builder=builder)
-  if debug:
-    print("Sharpness\t: " + txt)
+  # Sharpness Loop
+  txt = sharpness_loop(img)
+  if is_correct(txt):
+    return txt
 
+  # Cotrast Loop
+  txt = contrast_loop(img)
+  if is_correct(txt):
+    return txt
+
+  # Finally
   return txt
--- tdriver.py
+++ tdriver.py
@@ -1,17 +1,19 @@
 import ocr,base64,sys,re,os
 
 def parse_arg():
-  res = {'file':'all'}
+  res = {'file':'all','debug':False}
   if '--file' in sys.argv:
     i = sys.argv.index('--file')
     if len(sys.argv) < i + 1:
       exit(1)
     res['file'] = sys.argv[i + 1]
+  if '--detail' in sys.argv:
+    res['debug'] = True
   return res
 
-def exec_by_file(fpath):
+def exec_by_file(fpath,debug):
   b64str = open(fpath,'r').read()
-  txt = ocr.from_binary(b64str, debug=True)
+  txt = ocr.from_binary(b64str, debug=debug)
   print("THIS IS RESULT  : " + txt)
 
 def main():
@@ -19,10 +21,10 @@ def main():
   if args['file'] == 'all':
     for f_name in os.listdir('./sample'):
       print("===== " + f_name + " =====")
-      exec_by_file('./sample/' + f_name)
+      exec_by_file('./sample/' + f_name, args['debug'])
     return
 
-  exec_by_file(args['file'])
+  exec_by_file(args['file'], args['debug'])
 
 if __name__ == '__main__':
   main()
